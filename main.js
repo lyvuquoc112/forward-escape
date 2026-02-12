9aba6940eac4171485fcbdb6f2b7b9dfb34becb3
lyvuquoc112 <lyvuquoc112@gmail.com>
2026-02-12 16:48:30 +0700

    Block 1: Initial canvas setup with player render

---
+++ main.js
@@ -0,0 +1,10 @@
+﻿const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d"); // ngòi bút vẽ 2d
+
+const squareSize = 40;
+const x = (canvas.width - squareSize) / 2; // tính vị trí x
+const y = (canvas.height - squareSize) / 2; // tính vị trí y
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+ctx.fillStyle = "#4cc9f0"; // màu hình vẽ
+ctx.fillRect(x, y, squareSize, squareSize); // vị trí vẽ và kích thước của khối 
